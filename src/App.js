81229f9179150dc5d8251ab36e7f147434f48f1e
Sean Rose <sean1rose@gmail.com>
2016-08-26 21:34:33 -0700

    Add data csv file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
+import {csv} from 'd3-request';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentWillMount() {
+    csv('./data/birthdeathrates.csv', (error, data) => {
+      if (error) {
+        this.setState({loadError: true});
+      }
+      this.setState({
+        data: data.map(d => ({...d, x: Number(d.birth), y: Number(d.death)}))
+      });
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <div></div>
+    )
   }
 }
 
